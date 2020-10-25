02f34f6fa4c0e54f2a422fce4cb0a4a0e3452e44
Robert Geil <robert@rgeil.net>
2020-10-24 17:43:51 -0700

    Holy sh*t it finally worked, this was horrible! Added leaflet to display maps Added location for the user, updated on client side Figured out some horrible typescript import stuff, ugh

---
+++ web/src/view/page/MapPage.tsx
@@ -0,0 +1,49 @@
+import { RouteComponentProps } from '@reach/router'
+import * as React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type * as LT from 'react-leaflet'
+import { H1 } from '../../style/header'
+import { AppRouteParams } from '../nav/route'
+import { Page } from './Page'
+
+interface MapPageProps extends RouteComponentProps, AppRouteParams {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function MapPage(props: MapPageProps) {
+  let Leaflet: typeof LT | undefined = undefined
+  const [location, setLocation] = React.useState<[number, number]>([0, 0])
+  if (typeof window !== 'undefined') {
+    Leaflet = require('react-leaflet')
+  }
+  if (typeof navigator !== 'undefined') {
+    navigator.geolocation.getCurrentPosition(c => setLocation([c.coords.latitude, c.coords.longitude]))
+  }
+  return (
+    <Page>
+      <div id="map-container" style={{ height: 180 }}>
+        {!Leaflet ? (
+          <div>Loading</div>
+        ) : (
+          <Leaflet.Map center={location} zoom={13}>
+            <Leaflet.TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Leaflet.Marker position={location}>
+              <Leaflet.Popup>You are here!</Leaflet.Popup>
+            </Leaflet.Marker>
+          </Leaflet.Map>
+        )}
+      </div>
+      <button
+        onClick={() => {
+          console.log(window)
+        }}
+      >
+        CLICK ME
+      </button>
+      <button>bar</button>
+      <H1>Hello foo bar!</H1>
+      <div>BAZ</div>
+    </Page>
+  )
+}
