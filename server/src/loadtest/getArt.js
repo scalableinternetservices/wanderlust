62b6f5b6a30840f3fc89dd74b5033f457026d780
Timothy Rediehs <trediehs@g.ucla.edu>
2020-12-05 20:05:40 -0800

    add redis to store user session

---
+++ server/src/loadtest/getArt.js
@@ -0,0 +1,40 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+let host = `http://${__ENV.HOST || 'localhost:3000'}`
+function getUploadPage() {
+  let res = http.get(host)
+  check(res, { 'status was 200': r => r.status == 200 })
+}
+
+function uploadArtwork() {
+  let query = `
+    query arts {
+      arts {
+        name
+      }
+    }
+  `
+  let params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-authtoken': 'da1357db-14cd-4f62-9208-968869454750',
+    },
+  }
+  let res = http.post(`${host}/graphql`, JSON.stringify({ query: query }), params)
+  console.log(res.body)
+  check(res, { 'status was 200': r => r.status == 200 })
+}
+
+export let options = {
+  stages: [
+    { duration: '20s', target: 80 },
+    { duration: '1m', target: 80 },
+    { duration: '20s', target: 0 },
+  ],
+}
+
+export default function () {
+  getUploadPage()
+  sleep(Math.random() * 4)
+  uploadArtwork()
+}
