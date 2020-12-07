0d939d9863c6b6a5d64176ef60c781481da47c63
meghailango <megha.ilango@yahoo.com>
2020-12-07 04:56:53 -0800

    improving spacing/styling, added ability to enter name for art

--- web/src/view/page/UploadPage.tsx
+++ web/src/view/page/UploadPage.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { FetchUserContext } from '../../graphql/query.gen'
 import { PillButton } from '../../style/button'
 import { H2, H5 } from '../../style/header'
+import { Input } from '../../style/input'
 import { Spacer } from '../../style/spacer'
 import { fetchUser } from '../auth/fetchUser'
 import { AppRouteParams } from '../nav/route'
@@ -13,8 +14,9 @@ interface UploadPageProps extends RouteComponentProps, AppRouteParams {}
 
 export function UploadPage(props: UploadPageProps) {
   const [selectedFile, setFile] = React.useState('')
+  const [name, setName] = React.useState('')
   const { data } = useQuery<FetchUserContext>(fetchUser)
-  const name = (!!data && !!data.self) ? data.self.username : 'anonymous'
+  const author = !!data && !!data.self ? data.self.username : 'anonymous'
 
   const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -23,7 +25,9 @@ export function UploadPage(props: UploadPageProps) {
   }
 
   const fileUploadHandler = () => {
-    //send to backend??
+    //TODO: send to backend??
+    //i have the name, image URL
+    console.log(name)
   }
 
   return (
@@ -31,20 +35,39 @@ export function UploadPage(props: UploadPageProps) {
       <Spacer $h4 />
       <H2>upload artwork</H2>
       <Spacer $h4 />
-      <H5>name: {name}</H5>
       <div className="flex flex-column">
+        <H5>author: {author}</H5>
         <div className="flex justify-left">
-          {/* <PillButton $pillColor="white">
-          Attach File
-          </PillButton> */}
-          <input type="file" onChange={fileSelectedHandler}></input>
-          {/* <FileSelector onLoadFile={(files:FileList) => console.log(files)}/> */}
+          <H5>name: </H5>
+          <Input $onChange={setName} name="name" type="text" placeholder="enter" />
         </div>
         <Spacer $h2 />
+        <div className="flex-justify-left">
+          <H5>image: </H5>
+        </div>
+        <div className="flex justify-center">
+          <input type="file" onChange={fileSelectedHandler}></input>
+        </div>
+        <Spacer $h2 />
+        <br></br>
         <div className="flex justify-center">
           <img src={selectedFile}></img>
         </div>
-        <Spacer $h2 />
+        <Spacer $h1 />
+        {selectedFile == '' && (
+          <>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          </>
+        )}
+        <br></br>
         <div className="flex justify-center">
           <PillButton $pillColor="purple" onClick={fileUploadHandler}>
             Submit
