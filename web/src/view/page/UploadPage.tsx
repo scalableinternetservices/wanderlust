28cf93c6a672d50106463a65e6f2cc18ad1eb27f
meghailango <megha.ilango@yahoo.com>
2020-12-07 20:59:39 -0800

    got all the components to upload an image, just gotta figure out the mutation now

--- web/src/view/page/UploadPage.tsx
+++ web/src/view/page/UploadPage.tsx
@@ -4,7 +4,6 @@ import * as React from 'react'
 import { FetchUserContext } from '../../graphql/query.gen'
 import { PillButton } from '../../style/button'
 import { H2, H5 } from '../../style/header'
-import { ReactFileReader } from 'react-file-reader';
 import { Input } from '../../style/input'
 import { Spacer } from '../../style/spacer'
 import { fetchUser } from '../auth/fetchUser'
@@ -14,8 +13,9 @@ import { Page } from './Page'
 interface UploadPageProps extends RouteComponentProps, AppRouteParams {}
 
 export function UploadPage(props: UploadPageProps) {
-  const [selectedFile, setFile] = React.useState('')
-  // const [location, setLocation] = React.useState<{ lat: number; lng: number } | null>(null)
+  const [selected_file_url, setFileURL] = React.useState('')
+  const [image_string, setImageString] = React.useState('') //BASE64_ENCODED_STRING_OF_IMAGE_OR_TEXT
+  const [type, setType] = React.useState('') //accepted types are image/png, image/jpeg, and text/plain
   const [name, setName] = React.useState('')
   const { data } = useQuery<FetchUserContext>(fetchUser)
   const author = !!data && !!data.self ? data.self.username : 'anonymous'
@@ -28,8 +28,18 @@ export function UploadPage(props: UploadPageProps) {
   // }`
 
   const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(URL.createObjectURL(event.target.files[0]))
+    const files = event.target.files
+    if (files) {
+      setFileURL(URL.createObjectURL(files[0]))
+      setType(files[0].type)
+      const reader = new FileReader()
+      reader.onload = function (event) {
+        if (event && event.target && event.target.result) {
+          const binaryString = event.target.result as string
+          setImageString(btoa(binaryString))
+        }
+      }
+      reader.readAsBinaryString(files[0])
     }
   }
 
@@ -48,24 +58,25 @@ export function UploadPage(props: UploadPageProps) {
         lng = c.coords.longitude
       })
     }
+    console.log(lat)
+    console.log(lng)
     //creator id
     console.log(creator_id)
-    //type: image in png for now..TODO: add text option, support jpg
-    ImgToBase64.getBase64String(selectedFile)
-      .then(base64String => doSomethingWith(base64String))
-      .catch(err => doSomethingWith(err))
-    const art = {
-      art: {
-        name: name,
-        creatorId: creator_id, //get logged in user
-        location: {
-          lat: lat, //get location
-          lng: lng,
-        },
-        data: 'data:/image/png;base64, BASE64_ENCODED_STRING_OF_IMAGE_OR_TEXT',
-        //accepted types are image/png, image/jpeg, and text/plain
-      },
-    }
+    //type: based on file extension
+    console.log(type)
+    //string
+    console.log(image_string)
+    // const art = {
+    //   art: {
+    //     name: name,
+    //     creatorId: creator_id, //get logged in user
+    //     location: {
+    //       lat: lat, //get location
+    //       lng: lng,
+    //     },
+    //     data: 'data:/' + type + ';base64, ' + image_string,
+    //   },
+    // }
   }
 
   return (
@@ -76,7 +87,7 @@ export function UploadPage(props: UploadPageProps) {
       <div className="flex flex-column">
         <H5>author: {author}</H5>
         <div className="flex justify-left">
-          <H5>name: </H5>
+          <H5>title: </H5>
           <Input $onChange={setName} name="name" type="text" placeholder="enter" />
         </div>
         <Spacer $h2 />
@@ -89,10 +100,10 @@ export function UploadPage(props: UploadPageProps) {
         <Spacer $h2 />
         <br></br>
         <div className="flex justify-center">
-          <img src={selectedFile}></img>
+          <img src={selected_file_url}></img>
         </div>
         <Spacer $h1 />
-        {selectedFile == '' && (
+        {selected_file_url == '' && (
           <>
             <br></br>
             <br></br>
